a31b695513d4a5ab5b8436b8bb09295d3ddb941a
Ernesto Floriano Amorim <ernestofamorim@gmail.com>
2023-05-03 16:25:51 -0300

    commit inicial

---
+++ Counter/app.js
@@ -0,0 +1,10 @@
+let btnIncremento = document.querySelector(".btn-incremento");
+let btnReset = document.querySelector(".btn-reset");
+let btnDecre = document.querySelector(".btn-decremento");
+let numero = document.querySelector(".numero");
+
+btnIncremento.addEventListener("click", function () {
+  numero.innerHTML = parseInt(numero.innerText) + 1;
+});
+
+btnDecre.addEventListener("click", function () {});
